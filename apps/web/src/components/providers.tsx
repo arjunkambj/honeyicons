fa4d91924c457e317de7d4dd813cbfdb042b1103
Arjun Kamboj <arjunkamboj87699@gmail.com>
2026-09-12 18:48:08 +0530

    feat(web): add smooth scrolling with reduced motion support

--- apps/web/src/components/providers.tsx
+++ apps/web/src/components/providers.tsx
@@ -1,14 +1,33 @@
 "use client";
 
 import { Toaster } from "@honeyicons/ui/components/sonner";
+import { ReactLenis } from "lenis/react";
 
 import { ThemeProvider } from "./theme-provider";
 
 export default function Providers({ children }: { children: React.ReactNode }) {
-  return (
-    <ThemeProvider attribute="class" defaultTheme="system" enableSystem disableTransitionOnChange>
-      {children}
-      <Toaster richColors />
-    </ThemeProvider>
-  );
+	return (
+		<ThemeProvider
+			attribute="class"
+			defaultTheme="system"
+			enableSystem
+			disableTransitionOnChange
+		>
+			<ReactLenis
+				root
+				options={{
+					autoRaf: true,
+					lerp: 0.1,
+					anchors: true,
+					autoToggle: true,
+					allowNestedScroll: true,
+					stopInertiaOnNavigate: true,
+					respectReducedMotion: true,
+				}}
+			>
+				{children}
+			</ReactLenis>
+			<Toaster richColors />
+		</ThemeProvider>
+	);
 }
